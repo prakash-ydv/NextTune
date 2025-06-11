f0078d52239812776f311f043b2569265cd099b8
Prakash Kumar <prakashyadav9576@gmail.com>
2025-06-11 14:45:26 +0530

    room structure

---
+++ server/proto.js
@@ -0,0 +1,19 @@
+const room = {
+  roomId: {
+    roomInfo: {
+      roomName: "",
+      roomCode: "",
+      roomPass: "",
+      adminId: "",
+      mods: ["mod1", "mod1"],
+      createdTime: "",
+    },
+    usersInfo: [{ name: "", userId: "", joinedTime: "", role: "admin" }],
+    videoInfo: {
+      currentVideoId: "",
+      startedAt: "",
+      queue: [{ songs }],
+    },
+    chatInfo: [{ name: "", message: "", time: "", role: "" }],
+  },
+};
