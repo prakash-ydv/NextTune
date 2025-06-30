e0379b4e5958acd50e15434753221efacfa4ccb3
Prakash Kumar <prakashyadav9576@gmail.com>
2025-07-01 00:05:10 +0530

    add toast container

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RoomContextProvider } from "./context/RoomContext.jsx";
+import { ToastContextProvider } from "./context/ToastContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <RoomContextProvider>
-    <App />
+    <ToastContextProvider>
+      <App />
+    </ToastContextProvider>
   </RoomContextProvider>
 );
