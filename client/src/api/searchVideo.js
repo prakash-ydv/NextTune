d326701cdcafeffb3ddca245f0b84609f0f3a364
Prakash Kumar <prakashyadav9576@gmail.com>
2025-07-13 15:13:14 +0530

    api config for production

--- client/src/api/searchVideo.js
+++ client/src/api/searchVideo.js
@@ -1,7 +1,7 @@
 const api = import.meta.env.VITE_API;
 
 async function searchVideo(query) {
-  const response = await fetch(`http://localhost:3000/search/${query}`);
+  const response = await fetch(`${api}${query}`);
   const data = await response.json();
   return data;
 }
